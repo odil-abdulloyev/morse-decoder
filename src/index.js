7ac54b64b19eb842dfb1a3facde36a90c3581250
Odil Abdulloyev <abdulloyevodil@mail.ru>
2020-10-08 11:23:18 +0500

    feat: add solution for morse-decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,28 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let result = '';
+    let exprArr = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        exprArr.push(expr.substr(i, 10));
+    }
+    for (let i = 0; i < exprArr.length; ++i) {
+        if (exprArr[i] === '**********') {
+            result += ' ';
+            continue;
+        }
+        const letterCode = exprArr[i];
+        let morseCode = '';
+        for (let j = 0; j < letterCode.length; j += 2) {
+            if (letterCode[j] === '1' && letterCode[j + 1] === '0') {
+                morseCode += '.';
+            } else if (letterCode[j] === '1' && letterCode[j + 1] === '1') {
+                morseCode += '-';
+            }
+        }
+        result += MORSE_TABLE[morseCode];
+    }
+    return result;
 }
 
 module.exports = {
